b28c4b538740550007cb6b1343b17ee9504b3faa
杨智全 <328442720@qq.com>
2021-09-11 00:14:10 +0800

    first commit

---
+++ src/components/PassEdit/PassEdit.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="passedit">
+      <el-form class="passedit-form" ref="form" :model="form" label-width="100px">
+        <el-form-item label="新密码">
+          <el-input v-model="form.pass1" type="password" autocomplete="off"> </el-input>
+        </el-form-item>
+        <el-form-item label="确认新密码">
+          <el-input v-model="form.pass2" type="password" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="onSubmit">确认修改</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+</template>
+
+<script>
+  import {removeStore} from '../../config/mUtils'
+  import {reqEditPassword} from '../../api/index'
+    export default {
+      data() {
+        return {
+          form: {
+            pass1:'',
+            pass2:''
+          }
+        }
+      },
+      methods: {
+          onSubmit() {
+              this.$message({
+                  message: '密码已修改，请重新登录',
+                  type: 'success'
+              });
+                  removeStore("bbs_font_userid")
+                  removeStore("bbs_font_username")
+                  this.$store.dispatch('setIsLogin', false)
+                  this.$router.push('/')
+          }
+      }
+    }
+</script>
+
+<style>
+.passedit{
+  padding: 15px 10px;
+  min-height: 391px;
+}
+  .passedit-form{
+    margin-top: 15px;
+    width: 45%;
+  }
+</style>
---
+++ src/pages/Home/Home.vue
@@ -0,0 +1,101 @@
+<template>
+    <div class="home">
+      <img class="banner" src="../../assets/img/banner.jpg" alt="">
+      <!-- 组件化开发 -->
+      <div class="container">
+        <el-row class="center_content" :gutter="15">
+          <el-col :span="1">
+            &nbsp;
+          </el-col>
+          <!-- 左边菜单栏 -->
+          <el-col :span="2">
+            <div class="grid-content bg-purple">
+              <LeftMenu></LeftMenu>
+            </div>
+          </el-col>
+          <!-- 中间内容 -->
+          <el-col :span="15" class="article-content">
+            <div class="grid-content bg-purple">
+              <!--<ArticleTab :nowPage="1" @getHomeArticleList="getHomeArticleList"></ArticleTab>-->
+              <ArticleTab :nowPage="1"></ArticleTab>
+            </div>
+          </el-col>
+          <!-- 右边板块 -->
+          <el-col :span="5">
+            <!-- 头板块 -->
+            <div class="grid-content bg-purple">
+              <TopPlate></TopPlate>
+            </div>
+            <!-- 积分牛人板块 -->
+            <div class="grid-content bg-purple mg-top-15">
+              <TopIntegralUser></TopIntegralUser>
+            </div>
+            <div class="grid-content bg-purple mg-top-15">
+              <AboutUs></AboutUs>
+            </div>
+          </el-col>
+          <el-col :span="1">
+            <div class="grid-content bg-purple">
+              &nbsp;
+            </div>
+          </el-col>
+        </el-row>
+      </div>
+
+    </div>
+</template>
+
+<script>
+//  import '../../assets/css/reset.css'
+  import LeftMenu from '../../components/LeftMenu/LeftMenu.vue'
+  import ArticleTab from '../../components/ArticleTab/ArticleTab.vue'
+  import TopIntegralUser from '../../components/TopIntegralUser/TopIntegralUser.vue'
+  import AboutUs from '../../components/AboutUs/AboutUs.vue'
+  import TopPlate from '../../components/TopPlate/TopPlate.vue'
+  import {reqArticleList} from '../../api/index'
+  export default {
+    data(){
+      return {
+
+      }
+    },
+    components:{
+      LeftMenu,
+      ArticleTab,
+      TopIntegralUser,
+      AboutUs,
+      TopPlate
+    },
+    methods:{
+//      async getHomeArticleList(plateid,type,userid,pageNum){
+//        const result = await reqArticleList(plateid,type,userid,pageNum)
+//        if(type===1){
+//          this.$store.dispatch('getHomeNewArticleList',result.articleLists)
+//        }else{
+//          this.$store.dispatch('getHomeHotArticleList',result.articleLists)
+//        }
+//      }
+    }
+  }
+</script>
+
+<style>
+.banner{
+  width: 100%;
+  height: 120px;
+  margin-top: 5px;
+}
+.home{
+  background: #F5F6F7;
+}
+.container{
+  padding: 7.5px;
+}
+.center_content{
+  margin-top: 8px;
+}
+  .mg-top-15{
+    margin-top: 15px;
+  }
+</style>
+
